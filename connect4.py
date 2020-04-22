b4b13d5b1e041b98b7848069f316d9c2789cf1da
abhishek <Abhishek Kumar>
2020-04-22 04:42:39 -0400

    Added Minimax Agent

--- connect4.py
+++ connect4.py
@@ -1,38 +1,56 @@
+import minimax
+from importlib import reload
+reload(minimax)
 
-class Connect4():
+class Connect4:
     def __init__(self, oldGame = None):
         self.height = 6
         self.width = 7
         self.turn = 1
         self.gameOver = False
-
-        if oldGame == None:
-            self.newBoard()
-        else:
-            self.board = oldGame.board
-            self.column_counts = oldGame.column_counts
+        self.winner = 0
+        self.newBoard()
+        if oldGame is not None:
+            self.copyBoard(oldGame)
+            self.turn = oldGame.turn
 
     def newBoard(self):
         self.column_counts = [0 for _ in range(self.width)]
         self.board = [[0 for _ in range(self.width)] for __ in range(self.height)]
+    
+    def copyBoard(self, oldGame):
+        for r in range(self.height):
+            for c in range(self.width):
+                self.board[r][c] = oldGame.board[r][c]
+        self.countColumns()
+    
+    def countColumns(self):
+        for c in range(self.width):
+            count = 0
+            for r in range(self.height):
+                if self.board[r][c] != 0:
+                    count += 1
+            self.column_counts[c] = count
 
     def printBoard(self):
         for i in range(self.height):
             print(self.board[i])
+        print('\n')
     
-    def playTurn(self, column):
+    def playTurn(self, column, human):
         if column < 0 or column > self.width - 1:
-            print('Not a viable turn')
-            return
+            if human:
+                print('Not a viable turn')
 
         fill = self.column_counts[column]
         if fill > self.height - 1:
-            print('Column is already filled')
-            return
+            if human:
+                print('Column is already filled')
 
         self.board[self.height-1-fill][column] = self.turn
         self.column_counts[column] += 1
-        self.printBoard()
+        if human:
+            self.printBoard()
         self.changePlayer()
     
     def changePlayer(self):
@@ -43,10 +61,10 @@ class Connect4():
     
     def checkWin(self):
         forward, backward, vertical, horizontal = self.checkForwardDiag(), self.checkBackwardDiag(), self.checkVertical(), self.checkHorizontal()
-        winner = max(forward, backward, vertical, horizontal)
-        if winner != 0:
+        win = max(forward, backward, vertical, horizontal)
+        if win != 0:
             self.gameOver = True
-            print('Player ' + str(winner) + ' Won!')
+            self.winner = win   
     
     def checkForwardDiag(self):
         player = 0
@@ -61,11 +79,11 @@ class Connect4():
             
     def checkBackwardDiag(self):
         player = 0
-        for c in range(3, self.width):
-            for r in range(self.height-3):
+        for c in range(self.width-3):
+            for r in range(3, self.height):
                 if self.board[r][c] != 0:
                     player = self.board[r][c]
-                    if self.board[r-1][c-1] == player and self.board[r-2][c-2] == player and self.board[r-3][c-3] == player:
+                    if self.board[r-1][c+1] == player and self.board[r-2][c+2] == player and self.board[r-3][c+3] == player:
                         return player
                 player = 0
         return player
@@ -93,26 +111,31 @@ class Connect4():
         return player
     
     def botTurn(self):
-        best_move = 0
-        self.playTurn(best_move)
+        game = self
+        best_move = minimax.MinimaxAgent.runBot(0, self)
+        self.playTurn(best_move, False)
     
     def play2Player(self):
         while not self.gameOver:
             column = input('Enter column to insert piece:')
             column = int(column)
-            self.playTurn(column)
+            self.playTurn(column, True)
             self.checkWin()
+        print('Player ' + str(self.winner) + ' Won!')
     
     def play1Player(self):
         while not self.gameOver:
             if(self.turn == 1):
                 column = input('Enter column to insert piece:')
                 column = int(column)
-                self.playTurn(column)
+                self.playTurn(column, True)
                 self.checkWin()
             else:
+                print('Bot Turn')
                 self.botTurn()
+                self.printBoard()
                 self.checkWin()
+        print('Player ' + str(self.winner) + ' Won!')
 
 game = Connect4()
 game.play1Player()
--- minimax.py
+++ minimax.py
@@ -1,1 +1,96 @@
-def minimax():
+import connect4
+import random
+
+class MinimaxAgent:
+    def runBot(self, game):
+        return minimax(game, 5, -1 * float('inf'),float('inf'),True)[1]
+
+def minimax(game, depth, alpha, beta, player):
+    game.checkWin()
+    if depth == 0:
+        return scorer(game), None
+    if game.gameOver:
+        return terminalState(game)
+    if player:
+        return maxPlayer(game, depth, alpha, beta, player)
+    else:
+        return minPlayer(game, depth, alpha, beta, player)
+
+def scorer(game):
+    score = 0
+    for r in range(game.height):
+        for c in range(game.width-3):
+            score += evaluateArea(game.board[r][c:c+3])
+
+    for r in range(game.height-3):
+        for c in range(game.width):
+            score += evaluateArea([game.board[r][c], game.board[r+1][c], game.board[r+2][c], game.board[r+3][c]])
+
+    for r in range(game.height-3):
+        for c in range(game.width-3):
+            score +=  evaluateArea([game.board[r][c], game.board[r+1][c+1], game.board[r+2][c+2], game.board[r+3][c+3]])
+    
+    for r in range(3, game.height):
+        for c in range(game.width-3):
+            score +=  evaluateArea([game.board[r][c], game.board[r-1][c+1], game.board[r-2][c+2], game.board[r-3][c+3]])
+
+    return score
+
+def evaluateArea(locations):
+    bot, player = 0, 0
+    for location in locations:
+        if location == 1:
+            player += 1
+        if location == 2:
+            bot += 1
+    score =  5*bot - 2*player
+    return score
+
+def terminalState(game):
+    if game.winner == 1:
+        return (-10000000000, None)
+    if game.winner == 2:
+        return (10000000000, None)
+    else:
+        return (0, None)
+
+def viablePermuation(game):
+    columns = []
+    for i in range(game.width):
+        if game.column_counts[i] > game.height - 1:
+            continue
+        columns.append(i)
+    random.shuffle(columns)
+    return columns
+
+def maxPlayer(game, depth, alpha, beta, player):
+    column = 0
+    best_score = -1 * float('inf')
+    options = viablePermuation(game)
+    for option in options:
+        game_copy = connect4.Connect4(game)
+        game_copy.playTurn(option, False)
+        score = minimax(game_copy, depth-1, alpha, beta, False)[0]
+        if score > best_score:
+            best_score = score
+            column = option
+        alpha = max(alpha, score)
+        if alpha >= beta:
+            break
+    return (best_score, column)
+
+def minPlayer(game, depth, alpha, beta, player):
+    column = 0
+    best_score = float('inf')
+    options = viablePermuation(game)
+    for option in options:
+        game_copy = connect4.Connect4(game)
+        game_copy.playTurn(option, False)
+        score = minimax(game_copy, depth-1, alpha, beta, True)[0]
+        if score < best_score:
+            best_score = score
+            column = option
+        beta = min(beta, score)
+        if alpha >= beta:
+            break
+    return (best_score, column)
